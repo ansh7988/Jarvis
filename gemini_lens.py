f885d22ef4983dc5e6cadd1bb8f4ea39098e103c
Anshdeep Singh <anshdeep7988@gmail.com>
2026-08-02 19:41:17 +0530

    Implement image analysis using Gemini API

---
+++ gemini_lens.py
@@ -0,0 +1,25 @@
+from google import genai
+
+# Replace with your Gemini API Key
+API_KEY = "api"
+
+client = genai.Client(api_key=API_KEY)
+
+
+def analyze_image(image_path,prompt):
+
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[
+            prompt,
+            genai.types.Part.from_bytes(
+                data=image_bytes,
+                mime_type="image/jpeg"
+            )
+        ]
+    )
+
+    return response.text
