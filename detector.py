d0921853fd01a92f0a524824edfd477a3ebdf531
Anshdeep Singh <anshdeep7988@gmail.com>
2026-09-09 23:50:27 +0530

    updation in detector.py
    Removed unnecessary blank lines in detector.py

--- detector.py
+++ detector.py
@@ -3,9 +3,6 @@ from ultralytics import YOLO
 import time
 from gemini_lens import analyze_image
 #import ocr later
-
-
-
 # Load model only once
 model = YOLO("yolo11n.pt")
 def vision_search(user_prompt):
